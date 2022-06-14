0ecde209b191b2c6fb0f635cf1b56b46fda7aff7
JenyaEfremovaFBS <106988881+JenyaEfremovaFBS@users.noreply.github.com>
2022-06-14 21:38:05 +0300

    Четвертая задача

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let toLower = str.toLowerCase();
+  if (toLower.includes('1xbet') || 
+      toLower.includes('xxx'))
+      {
+          return true;
+      }
+  return false;
 }
