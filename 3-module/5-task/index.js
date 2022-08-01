2059429282d4f49f7d13c909afa5b5fce60c3e9a
JenyaEfremovaFBS <106988881+JenyaEfremovaFBS@users.noreply.github.com>
2022-08-01 18:07:06 +0300

    3 module 5 task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
-}
+  return str
+    .split(/[\s,]+/)
+    .reduce((result, current) => {
+      if (!isNaN(current) && current < result.min) {
+        result.min = Number(current);
+      }
+
+      if (!isNaN(current) && current > result.min) {
+        result.max = Number(current);
+      }
+
+      return result;
+    }, {min: null, max: null});
+  }
